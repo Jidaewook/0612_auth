3be40442def36aa95729bd38d15df18dd5b8b021
Jay <psatdoctor@naver.com>
2019-06-12 20:34:06 +0900

    Server-side Validation

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 module.exports = {
     signUp: async(req, res, next) => {
-        console.log('signUp()');
+        console.log('contents of req.value.body', req.value.body);
     }, 
     signIn: async (req, res, next) => {
         console.log('signIn()');
