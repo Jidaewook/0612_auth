6485be2b5f012bdaeba43748a7e69181a16e3ef9
Jay <psatdoctor@naver.com>
2019-06-12 20:23:51 +0900

    express router

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
 
 const app = express();
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//Routes
+app.use('/users', userRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
 
-console.log(`http://localhost${PORT}`);
+console.log(`http://localhost:${PORT}`);
