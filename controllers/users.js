e581584c3d8cd4ad1c4f5a4bd334d5261637f513
Jay <psatdoctor@naver.com>
2019-06-12 20:49:52 +0900

    Add signup function

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,27 @@
+const userModel = require('../models/user');
+
+
 module.exports = {
     signUp: async(req, res, next) => {
-        console.log('contents of req.value.body', req.value.body);
+        // console.log('contents of req.value.body', req.value.body);
+        const {email, password} = req.value.body;
+
+        //Check if there is a user with the same email
+        const foundUser = await userModel.findOne({email});
+        if(foundUser){
+            return res.status(403).json({error: 'Email is already in use'});
+        }
+
+        //Else Create new user 
+        const newUser = new userModel({email, password});
+        await newUser
+            .save()
+            .then(result => {
+                res.status(200).json({user: 'created'});
+            })
+            .catch(err => console.log(err));
+
+        //
     }, 
     signIn: async (req, res, next) => {
         console.log('signIn()');
